dfed4b96c0425903d8fdca25c5da5dd69d74a939
mhaddaou <mhaddaou@student.1337.ma>
2023-09-15 17:38:33 +0100

    update

--- backend/dist/Models/adminsModule.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const adminSchema_1 = __importDefault(require("../allSchema/adminSchema"));
-const adminModel = (0, mongoose_1.model)("admins", adminSchema_1.default);
-exports.default = adminModel;
--- backend/dist/Models/userModel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const userSchema_1 = require("../allSchema/userSchema");
-const userModel = (0, mongoose_1.model)("users", userSchema_1.userSchema);
-exports.default = userModel;
